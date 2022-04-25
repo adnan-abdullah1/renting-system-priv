0331574992eececef89dffd7be4566e3affbd847
addddd123 <adshe886@gmail.com>
2022-04-25 10:21:09 +0530

    schema

--- backend/models/bookings.js
+++ backend/models/bookings.js
@@ -1,28 +1,32 @@
 const mongoose = require('mongoose')
-const {Schema} = require('mongoose')
+const { Schema } = require('mongoose')
 
 
 const bookingSchema = new Schema({
 
-    landLordId:{
+    landLordId: {
         type: mongoose.Types.ObjectId,
     },
-    tenantId:{
+    tenantId: {
         type: mongoose.Types.ObjectId,
     },
-    roomId:{
+    roomId: {
         type: mongoose.Types.ObjectId,
     },
     bookedAt: {
-        type:Date,
-        default:Date.now
+        type: Date,
+        default: Date.now
     },
-    checkOutDate:{
-        type:Date,
-        default: ()=> new Date(+new Date() + 7*24*60*60*1000).toISOString()
-    }
+    checkOutDate: {
+        type: Date,
+        default: () => new Date(+new Date() + 7 * 24 * 60 * 60 * 1000).toISOString()
+    },
+    landLordChecked: {
+        type: Boolean,
+        default: false
+    },
 
-    
+
 })
 
 module.exports = mongoose.model('bookingDetails', bookingSchema)
