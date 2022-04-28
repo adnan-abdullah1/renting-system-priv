0a58d828de162cb7ef62143067ec1b0bb87807f0
addddd123 <adshe886@gmail.com>
2022-04-28 13:01:06 +0530

    removed unnecssary lines

--- frontend/src/app/modules/landlord/components/list-rent-details/list-rent-details.component.ts
+++ frontend/src/app/modules/landlord/components/list-rent-details/list-rent-details.component.ts
@@ -58,8 +58,6 @@ export class ListRentDetailsComponent implements OnInit {
     //call to backend
     this.rentService.getRentDetails().subscribe((res: any) => {
       console.log(res)
-      res.image=[]
-      res.legaalDocuments=[]
       this.dataSource = res;
       
     })
