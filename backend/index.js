f55f0192e8f6bf25ab7fbe0c66302b2c97fd5a47
Hanan11240 <peerzadahanannaseer@gmail.com>
2022-05-12 15:09:52 +0530

    'landlord-list-styling'

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express()
 const cors = require('cors')
 
@@ -16,10 +16,9 @@ mongoose.connect('mongodb://localhost:27017/Renting-system')
     .then(() => console.log('connected Db'))
     .catch(console.error)
 
-// app.use(express.json())
 app.use(cors())
-app.use(bodyParser({ limit: '50mb' }));
-// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+// app.use(bodyParser({ limit: '50mb' }));
+
 
 app.get('/', (req, res) => {
     res.send('Hi')
